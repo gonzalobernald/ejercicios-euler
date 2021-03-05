c4ea33c7c74ba7f2a1c90fe27dd4b51306de88e7
gonzalobernald <75706545+gonzalobernald@users.noreply.github.com>
2021-03-05 16:04:21 -0300

    Agrega funciones
    CalcularDivisores busca los divisores de un numero, y calcularnumerosprimos busca los numeros primos de un array de divisore, pero no funciona.
    NO SOLUIONA EL EJERCICIO

---
+++ main.js
@@ -0,0 +1,19 @@
+ function calcularDivisores (numero){
+    const divisores =[];
+    for (let i = 2; i<numero;i++) {
+        if (numero%i===0){
+            divisores.push(i);
+        } else {
+            continue;
+        }
+    }
+    return divisores;
+};
+console.log(calcularDivisores(200));
+
+function calcularNumerosPrimos(divisores){
+    divisores = divisores;
+    for (let i =divisores.length;i>0;i--) {
+        if
+    }
+}
