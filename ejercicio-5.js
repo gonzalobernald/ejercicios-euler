555d878beafa66fa4163046b523713b5007149fc
gonzalobernald <75706545+gonzalobernald@users.noreply.github.com>
2021-03-10 12:56:26 -0300

    Resuelve el ejercicio completo
    La mayor dificultad estuvo a la hora de determinar los parametros, que no era necesario realmente si no que se hizo con el fin de que no sea estatico.
    Despues me trabe con los valores del else

--- ejercicio-5.js
+++ ejercicio-5.js
@@ -4,3 +4,22 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 */
 
+function dividirEspecialEjercicioCinco(valorLimite, numeroInicial, valoresChequeo) {
+    let acumulador = numeroInicial;
+    for (let i = valoresChequeo; i <= valorLimite; i++) {
+        if (acumulador % i === 0) {
+            continue
+        } else {
+            i = valoresChequeo - 1;
+            acumulador++;
+        }
+    }
+    return acumulador;
+}
+
+console.log(dividirEspecialEjercicioCinco(20, 20, 2));
+function demostracion() {
+    for (let i = 2; i <= 20; i++) {
+        console.log(232792560 % i);
+    }
+}
